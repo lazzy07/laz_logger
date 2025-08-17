522c78227ee143957e56e83b39132f5d10673b04
lazzy07 <222lasantha@gmail.com>
2025-08-17 12:48:00 -0400

    Issue with the cstdio and formatting issues fixed and pragma once added to the Log.hpp

--- include/Log.hpp
+++ include/Log.hpp
@@ -1,23 +1,66 @@
+#pragma once
+
+#include <cstdio>
+#include <string>
+#ifndef CORE_LOGGER_NAME
+#define CORE_LOGGER_NAME "unknown"
+#endif
+
+#ifndef CLI_LOGGER_NAME
+#define CLI_LOGGER_NAME "unknown"
+#endif
+
 #include <spdlog/spdlog.h>
 
-#ifndef CORE_LOGGER_NAME
-  #define CORE_LOGGER_NAME "unknown"
-#endif
+#ifndef LOG_ERRORS_ONLY
+// Core
+#define CORE_TRACE(...)                                               \
+  do {                                                                \
+    auto _lg = ::spdlog::get(CORE_LOGGER_NAME);                       \
+    if (_lg) {                                                        \
+      _lg->trace(__VA_ARGS__);                                        \
+    }                                                                 \
+    auto _fl = ::spdlog::get(std::string(CORE_LOGGER_NAME) + "_log"); \
+    if (_fl) {                                                        \
+      _fl->trace(__VA_ARGS__);                                        \
+    }                                                                 \
+  } while (0)
 
-#ifndef CLI_LOGGER_NAME 
-  #define CLI_LOGGER_NAME "unknown"
-#endif
+#define CORE_DEBUG(...)                                               \
+  do {                                                                \
+    auto _lg = ::spdlog::get(CORE_LOGGER_NAME);                       \
+    if (_lg) {                                                        \
+      _lg->debug(__VA_ARGS__);                                        \
+    }                                                                 \
+    auto _fl = ::spdlog::get(std::string(CORE_LOGGER_NAME) + "_log"); \
+    if (_fl) {                                                        \
+      _fl->debug(__VA_ARGS__);                                        \
+    }                                                                 \
+  } while (0)
 
-#ifndef LOG_ERRORS_ONLY 
+#define CORE_INFO(...)                                                \
+  do {                                                                \
+    auto _lg = ::spdlog::get(CORE_LOGGER_NAME);                       \
+    if (_lg) {                                                        \
+      _lg->info(__VA_ARGS__);                                         \
+    }                                                                 \
+    auto _fl = ::spdlog::get(std::string(CORE_LOGGER_NAME) + "_log"); \
+    if (_fl) {                                                        \
+      _fl->info(__VA_ARGS__);                                         \
+    }                                                                 \
+  } while (0)
 
-// Core Logging functionality
-#define CORE_TRACE(...) if (spdlog::get(CORE_LOGGER_NAME) != nullptr) {spdlog::get(CORE_LOGGER_NAME)->trace(__VA_ARGS__); spdlog::get(std::string(CORE_LOGGER_NAME) + "_log")->trace(__VA_ARGS__);}
-
-#define CORE_DEBUG(...) if (spdlog::get(CORE_LOGGER_NAME) != nullptr) {spdlog::get(CORE_LOGGER_NAME)->debug(__VA_ARGS__); spdlog::get(std::string(CORE_LOGGER_NAME) + "_log")->debug(__VA_ARGS__);}
-
-#define CORE_INFO(...) if (spdlog::get(CORE_LOGGER_NAME) != nullptr) {spdlog::get(CORE_LOGGER_NAME)->info(__VA_ARGS__); spdlog::get(std::string(CORE_LOGGER_NAME) + "_log")->info(__VA_ARGS__);}
-
-#define CORE_WARN(...) if (spdlog::get(CORE_LOGGER_NAME) != nullptr) {spdlog::get(CORE_LOGGER_NAME)->warn(__VA_ARGS__); spdlog::get(std::string(CORE_LOGGER_NAME) + "_log")->warn(__VA_ARGS__);}
+#define CORE_WARN(...)                                                \
+  do {                                                                \
+    auto _lg = ::spdlog::get(CORE_LOGGER_NAME);                       \
+    if (_lg) {                                                        \
+      _lg->warn(__VA_ARGS__);                                         \
+    }                                                                 \
+    auto _fl = ::spdlog::get(std::string(CORE_LOGGER_NAME) + "_log"); \
+    if (_fl) {                                                        \
+      _fl->warn(__VA_ARGS__);                                         \
+    }                                                                 \
+  } while (0)
 
 #else
 #define CORE_TRACE(...) (void)0
@@ -26,17 +69,75 @@
 #define CORE_WARN(...) (void)0
 #endif
 
-#define CORE_ERROR(...) if (spdlog::get(CORE_LOGGER_NAME) != nullptr) {spdlog::get(CORE_LOGGER_NAME)->error(__VA_ARGS__); spdlog::get(std::string(CORE_LOGGER_NAME) + "_log")->error(__VA_ARGS__);}
+#define CORE_ERROR(...)                                               \
+  do {                                                                \
+    auto _lg = ::spdlog::get(CORE_LOGGER_NAME);                       \
+    if (_lg) {                                                        \
+      _lg->error(__VA_ARGS__);                                        \
+    }                                                                 \
+    auto _fl = ::spdlog::get(std::string(CORE_LOGGER_NAME) + "_log"); \
+    if (_fl) {                                                        \
+      _fl->error(__VA_ARGS__);                                        \
+    }                                                                 \
+  } while (0)
 
-#define CORE_FATAL(...) if (spdlog::get(CORE_LOGGER_NAME) != nullptr) {spdlog::get(CORE_LOGGER_NAME)->critical(__VA_ARGS__); spdlog::get(std::string(CORE_LOGGER_NAME) + "_log")->critical(__VA_ARGS__);}
+#define CORE_FATAL(...)                                               \
+  do {                                                                \
+    auto _lg = ::spdlog::get(CORE_LOGGER_NAME);                       \
+    if (_lg) {                                                        \
+      _lg->critical(__VA_ARGS__);                                     \
+    }                                                                 \
+    auto _fl = ::spdlog::get(std::string(CORE_LOGGER_NAME) + "_log"); \
+    if (_fl) {                                                        \
+      _fl->critical(__VA_ARGS__);                                     \
+    }                                                                 \
+  } while (0)
 
+// CLI
+#define CLI_TRACE(...)                                               \
+  do {                                                               \
+    auto _lg = ::spdlog::get(CLI_LOGGER_NAME);                       \
+    if (_lg) {                                                       \
+      _lg->trace(__VA_ARGS__);                                       \
+    }                                                                \
+    auto _fl = ::spdlog::get(std::string(CLI_LOGGER_NAME) + "_log"); \
+    if (_fl) {                                                       \
+      _fl->trace(__VA_ARGS__);                                       \
+    }                                                                \
+  } while (0)
 
+#define CLI_INFO(...)                                                \
+  do {                                                               \
+    auto _lg = ::spdlog::get(CLI_LOGGER_NAME);                       \
+    if (_lg) {                                                       \
+      _lg->info(__VA_ARGS__);                                        \
+    }                                                                \
+    auto _fl = ::spdlog::get(std::string(CLI_LOGGER_NAME) + "_log"); \
+    if (_fl) {                                                       \
+      _fl->info(__VA_ARGS__);                                        \
+    }                                                                \
+  } while (0)
 
-//CLI Logging functionality
-#define CLI_TRACE(...) if (spdlog::get(CLI_LOGGER_NAME) != nullptr) {spdlog::get(CLI_LOGGER_NAME)->trace(__VA_ARGS__); spdlog::get(std::string(CLI_LOGGER_NAME) + "_log")->trace(__VA_ARGS__);}
+#define CLI_DEBUG(...)                                               \
+  do {                                                               \
+    auto _lg = ::spdlog::get(CLI_LOGGER_NAME);                       \
+    if (_lg) {                                                       \
+      _lg->debug(__VA_ARGS__);                                       \
+    }                                                                \
+    auto _fl = ::spdlog::get(std::string(CLI_LOGGER_NAME) + "_log"); \
+    if (_fl) {                                                       \
+      _fl->debug(__VA_ARGS__);                                       \
+    }                                                                \
+  } while (0)
 
-#define CLI_INFO(...) if (spdlog::get(CLI_LOGGER_NAME) != nullptr) {spdlog::get(CLI_LOGGER_NAME)->info(__VA_ARGS__); spdlog::get(std::string(CLI_LOGGER_NAME) + "_log")->info(__VA_ARGS__);}
-
-#define CLI_DEBUG(...) if (spdlog::get(CLI_LOGGER_NAME) != nullptr) {spdlog::get(CLI_LOGGER_NAME)->debug(__VA_ARGS__); spdlog::get(std::string(CLI_LOGGER_NAME) + "_log")->debug(__VA_ARGS__);}
-
-#define CLI_ERROR(...) if (spdlog::get(CLI_LOGGER_NAME) != nullptr) {spdlog::get(CLI_LOGGER_NAME)->trace(__VA_ARGS__); spdlog::get(std::string(CLI_LOGGER_NAME) + "_log")->trace(__VA_ARGS__);}
+#define CLI_ERROR(...)                                               \
+  do {                                                               \
+    auto _lg = ::spdlog::get(CLI_LOGGER_NAME);                       \
+    if (_lg) {                                                       \
+      _lg->error(__VA_ARGS__);                                       \
+    }                                                                \
+    auto _fl = ::spdlog::get(std::string(CLI_LOGGER_NAME) + "_log"); \
+    if (_fl) {                                                       \
+      _fl->error(__VA_ARGS__);                                       \
+    }                                                                \
+  } while (0)
--- include/lazlogger/LoggerManager.hpp
+++ include/lazlogger/LoggerManager.hpp
@@ -1,34 +1,36 @@
 #pragma once
 
+#include <cstdio>
+#include <filesystem>
 #include <memory>
+#include <string>
+// Keep it here: after cstdio and string
 #include <spdlog/spdlog.h>
 
-#include <filesystem>
+namespace LazLogger {
+enum class LOGLEVEL { TRACE, INFO, WARN, ERR, FATAL };
 
-namespace LazLogger{
-  enum class LOGLEVEL{
-    TRACE,
-    INFO,
-    WARN,
-    ERR,
-    FATAL
-  };
+struct LoggerSpec {
+  std::string name;
+  std::filesystem::path file;
+};
+struct LoggerInit {
+  LoggerSpec core;
+  LoggerSpec cli;
+};
 
-  struct LoggerSpec { std::string name; std::filesystem::path file; };
-  struct LoggerInit { LoggerSpec core; LoggerSpec cli; };
+class LoggerManager {
+ public:
+  void Initialize(const LoggerInit &opts);
+  void Terminate();
 
-  class LoggerManager{
-    public:
-      void Initialize(const LoggerInit& opts);
-      void Terminate();
+  void SetCliLoggerLevel(LOGLEVEL loggerLevel);
+  void SetCoreLoggerLevel(LOGLEVEL loggerLevel);
 
-      void SetCliLoggerLevel(LOGLEVEL loggerLevel);
-      void SetCoreLoggerLevel(LOGLEVEL loggerLevel);
-
-    public:
-      std::shared_ptr<spdlog::logger> m_CoreLogger;
-      std::shared_ptr<spdlog::logger> m_CoreFileLogger;
-      std::shared_ptr<spdlog::logger> m_CliLogger;
-      std::shared_ptr<spdlog::logger> m_CliFileLogger;
-  };
-}
+ public:
+  std::shared_ptr<spdlog::logger> m_CoreLogger;
+  std::shared_ptr<spdlog::logger> m_CoreFileLogger;
+  std::shared_ptr<spdlog::logger> m_CliLogger;
+  std::shared_ptr<spdlog::logger> m_CliFileLogger;
+};
+}  // namespace LazLogger
